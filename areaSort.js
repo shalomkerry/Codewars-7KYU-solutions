45a3086385537f72a27f2fa507ced6bb50bf742c
shalomkerry <shalombadeg8@gmail.com>
2024-05-25 18:58:24 +0300

    edited

--- areaSort.js
+++ areaSort.js
@@ -1,14 +1,17 @@
 // https://www.codewars.com/kata/5a1ebc2480171f29cf0000e5/train/javascript
-function sortArea(array) {
-  let rectangleArray = array.filter((x) => Array.isArray(x));
-  let circleArray = array.filter((x) => !Array.isArray(x));
-  let rectangleArea = rectangleArray.map((subArr) =>
-    subArr.reduce((acc, number) => acc * number, 1)
-  );
-  let circleArea = circleArray.map((x) => Math.PI * Math.pow(x, 2));
-  let arrays = rectangleArea.concat(circleArea);
-  arrays = arrays.map((x) => Math.floor(x));
 
-  console.log(arrays.sort((a, b) => a - b));
+function sortArea(array) {
+  let shapeswithArea = [];
+  array.forEach((element) => {
+    if (Array.isArray(element)) {
+      let rectangleArea = element.reduce((acc, number) => acc * number);
+      shapeswithArea.push({ shape: element, area: rectangleArea });
+    } else {
+      let circleArea = Math.PI * Math.pow(element, 2);
+      shapeswithArea.push({ shape: element, area: circleArea });
+    }
+    shapeswithArea.sort((a, b) => a.area - b.area);
+  });
+  console.log(shapeswithArea.map((item) => item.shape));
 }
-sortArea([[4, 6], 1.23, 3.444, [1.342, 3.212]]);
+sortArea([[4, 6], [1, 3], 2, 3, 10, [5, 7]]);
